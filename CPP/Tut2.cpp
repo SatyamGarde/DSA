7b74d7a5c38243bc4d06334c21ce6cdbd733402e
Satyam <satyamgarde30@gmail.com>
2022-08-26 00:05:14 +0530

    If else

---
+++ CPP/Tut2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the val of A"<<endl;
+    cin>>a;
+    cout<<"enter the val of B"<<endl;
+    cin>>b;
+    if(a>b){
+        cout<<"A is Greater than B"<<endl;
+    }
+    else
+    {
+        cout<<"B is Greater than A"<<endl;
+    }
+    return 0;
+}
