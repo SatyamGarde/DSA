9b48ef1765832af637d4c131cccbf1e0fe009888
Satyam <satyamgarde30@gmail.com>
2022-08-31 09:03:24 +0530

    Patterns

---
+++ Patterns/Tut10.cpp
@@ -0,0 +1,23 @@
+/* 111
+   222
+   333 */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the value" << endl;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << i;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+}
---
+++ Patterns/Tut9.cpp
@@ -0,0 +1,26 @@
+// Printing stars
+/*
+***
+***
+***
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the value" << endl;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << "*";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+}
