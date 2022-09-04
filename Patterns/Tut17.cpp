a7223b5f9e18215112c725be8ae5a91d5d11173f
Satyam <satyamgarde30@gmail.com>
2022-09-04 10:33:56 +0530

    pattern

---
+++ Patterns/Tut17.cpp
@@ -0,0 +1,30 @@
+/*
+1
+2 3
+3 4 5
+4 5 6 7
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value"<<endl;
+    cin>>n;
+    int row=1;
+    while (row<=n)
+    {
+        int colmn =1;
+        int val =row;
+        while (colmn<=row)
+        {
+            cout<<val<<" ";
+            val= val + 1;
+            colmn=colmn+1;
+        }
+        cout<<endl;
+        row= row +1;
+
+    }
+    
+}
