382666fca6bb3d98af88b9f14c9144bc5c6ebf90
Satyam <satyamgarde30@gmail.com>
2022-12-02 21:47:55 +0530

    CPP

---
+++ CPP/Tut37.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    for (int i = 0; i<=n; i++ )   
+    {
+        cout<<i<<endl;
+    }
+    int i=0;
+    while (i<=n)
+    {
+        cout<<i<<endl;
+        i=i+1;
+    }
+    
+    
+}
